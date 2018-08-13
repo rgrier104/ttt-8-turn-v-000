40133915dc0ee8e886c61ad4964941d2bd169a21
Rachel Grier <rgrier104@gmail.com>
2018-08-13 01:54:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,6 +36,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
+    display_board(board)
   else
     until valid_move?(board, index)
       turn(board)
